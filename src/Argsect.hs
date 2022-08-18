788e5f7e12a788740f24b5e89e63ab15a62f6c22
Eavolution <afkrobertson@proton.me>
2022-08-18 17:30:27 +0100

    Initial functional commit

--- src/Argsect.hs
+++ src/Argsect.hs
@@ -1,36 +1,21 @@
 module Argsect where
+import Types
 
-data Switch = 
-    UndefinedSwitch {id :: String} |
-    Switch { 
-            idShort :: String,
-            idLong :: String,
-            info :: String
-            }
+import Data.Maybe
 
-instance Show Switch where
-    show (Switch idS idL inf) = 
-        idS ++ " = " ++ idL ++ " -> " ++ inf
-    show (UndefinedSwitch switchId) = "Undefined switch = " ++ switchId
+-- Testing
+swHelp = Switch "-h" "--help" "Displays the help menu."
+swEu = Switch "-e" "--europe" "Formats EU style."
 
-instance Eq Switch where
-    -- If one of the id's are the same, the switch is the same
-    (==) (Switch idS1 idL1 _) (Switch idS2 idL2 _) = (idS1 == idS2) || (idL1 == idL2)
-    -- All undefineds are the same
-    (==) (UndefinedSwitch _) (UndefinedSwitch _) = True
-    -- If they're different, theyre not equivalent
-    (==) _ _ = False
-
-type PosArg = String
-
--- Arguments, fst positional in order, snd switches 
-type Args = ([PosArg], [Switch])
+dswWords = DataSwitch "-w" "--wordlist" Nothing True "Sets wordlist."
+parse = argsect [swHelp, swEu] [dswWords]
+-- /Testing
 
 getUndefined :: [Switch] -> [Switch]
 getUndefined = filter (\x -> x == UndefinedSwitch "")
 
 strCmpSwitch :: String -> Switch -> Bool
-strCmpSwitch str sw = (str == (idShort sw)) || (str == (idLong sw))
+strCmpSwitch str sw = (str == (swIdShort sw)) || (str == (swIdLong sw))
 
 -- Head that doesn't throw exceptions, but returns a Maybe
 head' :: [a] -> Maybe a
@@ -44,33 +29,49 @@ lstrip stripVal lst =
         [] -> []
         (x:xs) -> if x == stripVal
         then lstrip stripVal xs else lst
-        
-isSwitch :: String -> Bool
-isSwitch x
-    | null x = False
-    | head x == '-' = True
-    | otherwise = False
-    
+
+splitAtFirst :: Char -> String -> (String, String)
+splitAtFirst x = fmap (drop 1) . break (x ==)    
+
 -- Takes a list of defined switches and a potential switch. Matches the potential switch to an element
 -- of the defined switches. If its not found, then an UndefinedSwitch is returned.
 stringToSwitch :: [Switch] -> String -> Switch
-stringToSwitch switches string = maybe (UndefinedSwitch string) (\x -> x) $ strTomSwitch switches string
+stringToSwitch switches string = fromMaybe (UndefinedSwitch string) $ strTomSwitch switches string
     where 
         -- Converts a string to a Maybe Switches. If the string is a defined switch, then the Just of
         -- that is returned. Otherwise Nothing is returned
         strTomSwitch :: [Switch] -> String -> Maybe Switch
         strTomSwitch switches string = head' $ filter (strCmpSwitch string) switches
 
+stringToDataSwitch :: [DataSwitch] -> String -> DataSwitch
+stringToDataSwitch dSwitches string = fromMaybe (UndefinedDataSwitch string) (strTomSwitch dSwitches (fst split) (snd split))
+    where
+        getMatch :: String -> Maybe DataSwitch
+        getMatch str = head' (filter (\x -> (dswIdShort x == str) || (dswIdLong x == str)) dSwitches)
+
+        split = splitAtFirst '=' string
+        strTomSwitch :: [DataSwitch] -> String -> String-> Maybe DataSwitch
+        strTomSwitch switches string dat =
+            if isJust $ getMatch string then do
+                match <- (getMatch string)
+                Just (DataSwitch (dswIdShort match) (dswIdLong match) (Just     dat) (dswrequired match) (dswInfo match))
+            else
+                Nothing
+
+-- Classifies string into what type of argument it is
+classify :: String -> ArgClassification
+classify arg
+    | '-' /= (head arg) = ClPosArg
+    | '=' `elem` arg = ClDataSwitch
+    | otherwise = ClSwitch
 
 -- Takes the command line arguments and turns them into Args
-argsect :: [Switch] -> [String] -> Args
-argsect switches args = (fst lstTuple, stringsToSwitches switches potSwitches)
-    where 
-        stringsToSwitches :: [Switch] -> [String] -> [Switch]
-        stringsToSwitches sws = map (stringToSwitch sws)
-
-        -- lstTuple is a tuple of list of positional arguments and potentially defined switches
-        posArgs = [x | x <- args, not $ isSwitch x]
-        potSwitches = [y | y <- args, isSwitch y]
-        lstTuple = (posArgs, potSwitches) :: ([String], [String])
+argsect :: [Switch] -> [DataSwitch] -> [String] -> Args
+argsect switches dataSwitches args =
+    Args posArgs (map (stringToSwitch switches) potSwitches) (map (stringToDataSwitch dataSwitches) potDataSwitches)
+        where
+            -- lstTuple is a tuple of list of positional arguments, potentially defined switches
+            posArgs = [x | x <- args, ClPosArg == (classify x)]
+            potSwitches = [x | x <- args, ClSwitch == (classify x)]
+            potDataSwitches = [x | x <- args, ClDataSwitch == (classify x)]
         
---
+++ src/Types.hs
@@ -0,0 +1,64 @@
+-- Holds all the types and instances needed to parse arguments
+
+module Types where 
+import Data.Maybe
+
+type PosArg = String
+
+-- Describes what a string is
+data ArgClassification = ClSwitch | ClDataSwitch | ClPosArg deriving Eq
+
+-- Represents the command line arguments
+data Args = Args {
+            positionals :: [PosArg],
+            switches :: [Switch],
+            dataSwitches :: [DataSwitch]
+            }
+instance Show Args where
+    show (Args pos sw dsw) = "Positionals: " ++ (show pos) ++ "\nSwitches: " ++ (show sw) ++ "\nData switches: " ++ (show dsw)
+
+-- Represents a switch containing no data ie. Switch "-h" "--help" "Displays the help menu."
+data Switch =   UndefinedSwitch {swId :: String} |
+                Switch  {
+                        swIdShort :: String,
+                        swIdLong :: String,
+                        swInfo :: String
+                        }
+
+instance Show Switch where
+    show (Switch idS idL inf) = 
+        idS ++ " " ++ idL ++ " -> " ++ inf
+    show (UndefinedSwitch switchId)=
+        "Undefined switch: " ++ switchId
+
+instance Eq Switch where
+    -- If one of the id's are the same, the switch is the same
+    (==) (Switch idS1 idL1 _) (Switch idS2 idL2 _) = (idS1 == idS2) || (idL1 == idL2)
+    -- All undefineds are the same
+    (==) (UndefinedSwitch _) (UndefinedSwitch _) = True
+    -- If they're different, theyre not equivalent
+    (==) _ _ = False
+
+-- Represents a data switch (for example --wordlist=/usr/share/wordlists/rockyou.lst)
+data DataSwitch = UndefinedDataSwitch {dswId :: String} |
+                  DataSwitch {
+                            dswIdShort :: String,
+                            dswIdLong :: String,
+                            dswData :: Maybe String, -- The data for the switch, after the =
+                            dswrequired :: Bool, -- If the switch is required
+                            dswInfo :: String
+                            }
+
+
+instance Show DataSwitch where
+    show (DataSwitch idS idL dat req inf) = 
+        idS ++ " " ++ idL ++ " -> " ++ inf ++ " Required = " ++ show req ++ (maybe "." (\x -> ". Data = " ++ x) dat)
+    show (UndefinedDataSwitch switchId) = "Undefined data-switch: " ++ switchId
+
+instance Eq DataSwitch where
+    -- If one of the id's are the same, the switch is the same
+    (==) (DataSwitch idS1 idL1 _ _ _) (DataSwitch idS2 idL2 _ _ _) = (idS1 == idS2) || (idL1 == idL2)
+    -- All undefineds are the same
+    (==) (UndefinedDataSwitch _) (UndefinedDataSwitch _) = True
+    -- If they're different, theyre not equivalent
+    (==) _ _ = False
